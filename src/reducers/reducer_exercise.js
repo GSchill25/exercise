6bc2a05b015c24a5cc9432dbc3534c0367276df4
GSchill25 <schill1025@gmail.com>
2016-09-12 22:48:33 -0400

    reverse order of table

--- src/reducers/reducer_exercise.js
+++ src/reducers/reducer_exercise.js
@@ -4,7 +4,7 @@ import { FETCH_EXERCISE } from '../actions/index';
 export default function(state = [], action) {
 	switch (action.type) {
 	case FETCH_EXERCISE:
-	  return [ action.payload, ...state ]; // [exercise, exercise, exercise]
+	  return [...state, action.payload]; // [exercise, exercise, exercise]
 	}
 	return state;
 } 
